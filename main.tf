b7c50f968880fc382e36eef4fe933cbbe57aecb5
Vikgramm <vikgramm97.music@gmail.com>
2022-08-29 15:02:11 +0800

    Add storage account to storage remote state

--- main.tf
+++ main.tf
@@ -8,6 +8,13 @@ terraform {
   }
 
   required_version = ">= 1.1.0"
+
+  backend "azurerm" {
+        resource_group_name  = "Vik-tf-rg"
+        storage_account_name = "vikremotestorage"
+        container_name       = "vikstrgcontainer"
+        key                  = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
@@ -17,4 +24,23 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "Vik-tf-rg"
   location = "southeastasia"
+}
+
+resource "azurerm_storage_account" "rg" {
+  name                     = "vikremotestorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  ##allow_blob_public_access = true **No longer supported by Azure this syntax. This Access is automatically enabled.
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "rg" {
+  name                  = "vikstrgcontainer"
+  storage_account_name  = azurerm_storage_account.rg.name
+  container_access_type = "blob"
 }
